e2d546f7312891e5705a472a782dddfc7fdd17f7
Mehadi Hassan Siddique <mehadi.hassan2744@gmail.com>
2026-02-06 22:19:18 +0530

    Made pages

---
+++ NeuralX/src/Pages/TempAnalytics.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../components/NavBar/NavBar.jsx'
+
+function TempAnalytics() {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+}
+
+export default TempAnalytics;
